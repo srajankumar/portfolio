56dde29f8d5eb5fe4ef1ca8a097d64ec21d709c2
srajankumar <kumarsrajan02@gmail.com>
2023-07-19 15:46:13 +0530

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+    ? `${process.env.NEXT_PUBLIC_BASE_PATH}/`
+    : "",
+};
