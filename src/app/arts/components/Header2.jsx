e33368e3ce96eaca7ba538e7dc6a8862972ddf26
srajankumar <kumarsrajan02@gmail.com>
2023-07-22 21:03:19 +0530

    arts test

---
+++ src/app/arts/components/Header2.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div className=" pt-5">
+      <h1 className="font-[800] tracking-tight py-20 text-center text-[5rem] bg-[#f1f1f1] text-black">
+        Reo Arts
+        {/* <div className="aurora">
+          <div className="aurora__item"></div>
+          <div className="aurora__item"></div>
+          <div className="aurora__item"></div>
+          <div className="aurora__item"></div>
+        </div> */}
+      </h1>
+      {/* <p>{"A small collection of my works :)"}</p> */}
+    </div>
+  );
+};
+
+export default Header;
--- src/app/arts/page.tsx
+++ src/app/arts/page.tsx
@@ -2,12 +2,22 @@ import "./arts.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Gallery1 from "./components/Gallery1";
+import Footer2 from "./components/Footer2";
+import Header2 from "./components/Header2";
+import Gallery2 from "./components/Gallery2";
 const Arts = () => {
   return (
     <div>
-      <Header />
-      <Gallery1 />
-      <Footer />
+      <div>
+        <Header />
+        <Gallery1 />
+        <Footer />
+      </div>
+      <div>
+        <Header2 />
+        <Gallery2 />
+        <Footer2 />
+      </div>
     </div>
   );
 };
