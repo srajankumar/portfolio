7206dd34adbfec1e47d177b5f99f063287df8af1
srajankumar <kumarsrajan02@gmail.com>
2023-07-22 10:33:30 +0530

    revert layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+// import DarkMode from "@/components/DarkMode";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <DarkMode /> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
