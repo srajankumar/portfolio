297e283d705b4d945d2b1906719393bf75e0cd36
srajankumar <kumarsrajan02@gmail.com>
2023-07-22 10:31:19 +0530

    some error in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import DarkMode from "@/components/DarkMode";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <DarkMode /> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
